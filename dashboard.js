4abcdf0949100ba64927d4b8d8f179f4b9635c47
Yosber Guerrero <yosbergc@gmail.com>
2023-12-29 22:25:25 -0400

    Features
    Features:
    1) Songs added with JS.

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,5 @@
 let welcomeMessage = document.querySelector('.welcome-user');
+let songsContainer = document.querySelector('.latest-songs');
 window.addEventListener('load', iniciarSistema);
 
 function iniciarSistema() {
@@ -7,5 +8,16 @@ function iniciarSistema() {
     agregarCanciones();
 }
 function agregarCanciones() {
-    
+    for (song of todasCanciones) {
+        let {nombre, songArtwork} = song;
+        let songContainer = document.createElement('div')
+        songContainer.classList.add('song')
+        let imagen = document.createElement('img');
+        imagen.setAttribute('src', songArtwork);
+        let songNameContainer = document.createElement('h3');
+        songNameContainer.innerHTML = nombre;
+        songNameContainer.classList.add('song-name')
+        songContainer.append(imagen, songNameContainer);
+        songsContainer.append(songContainer)
+    }
 }
